9237e436bb68f71a36ac7222d5ef603e745baccf
Ike Ku <dempfi@yahoo.com>
2016-05-22 21:15:08 +0300

    styling for main

--- app/pages/login/index.js
+++ app/pages/login/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {Link} from 'react-router';
+import Rollover from 'components/rollover';
 
 export default class Login extends React.Component {
   render() {
     return (
       <div>
-        <div className='header'>Log in with your VChain ID</div>
+        <header>Log in with your VChain ID</header>
+        <div className='body'>
+          <label>Your Email</label>
+          <input type='email'/>
+
+          <label>Password</label>
+          <input type='password'/>
+          <a>I forgot</a>
+          <span className='notice'>
+            The ONLY information received by PlayBoy Inc.:
+            <strong>Yes or No that you are over 18</strong>
+          </span>
+
+          <Rollover title='Yes, really — nothing else' content='VChain doesn’t store any of the personal information you provided, therefore there is no way for anyone to ‘hack’ or get access to it any other way using our sistem.
+  That is also a reason why we can not recreate your personal ID in case you lose it.'/>
+
+          <button>Enter PlayBoy</button>
+
+          <Link className='link-button' to='/apply'>
+            Get a VChain ID
+          </Link>
+        </div>
       </div>
     );
   }
